8eb0c2d4bcb5b028a6f6fa2514e184d1f88ad2f7
Tjerk <tjerk.schuringa@gmail.com>
2020-03-06 19:05:17 +0100

    corrected function issues

--- robot_arm_control.py
+++ robot_arm_control.py
@@ -6,50 +6,19 @@ from __future__ import division
 # Uncomment to enable debug output.
 import logging
 
-# Import the PCA9685 module.
-import Adafruit_PCA9685
-
 # Robot arm functions
 import robot_arm_functions
-
-# Configure min and max servo pulse lengths
-# MG946R
-servo_min_mg946r = 90  # Min pulse length out of 4096
-servo_max_mg946r = 600  # Max pulse length out of 4096
-
-# SG60
-servo_min_sg90 = 146  # Min pulse length out of 4096
-servo_max_sg90 = 600  # Max pulse length out of 4096
-
-channel_gripper = 3
-channel_arm_under = 7
-channel_arm_above = 11
-
-base_arm_under = 20
-base_arm_above = 50
-
-max_arm_under = 100
-max_arm_above = 120
-
-# Initialise the PCA9685 using the default address (0x40)
-# Alternatively specify a different address and/or bus:
-# pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
-pwm = Adafruit_PCA9685.PCA9685()
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
     # Initialize starting point
-    current_angle_arm_under = base_arm_under
-    current_angle_arm_above = base_arm_above
-
-    # Set frequency to 60hz, good for servos.
-    pwm.set_pwm_freq(60)
+    current_angle_arm_under = robot_arm_functions.base_arm_under()
+    current_angle_arm_above = robot_arm_functions.base_arm_above()
 
     # If not on Base, Go direct to base (not using go_arm_by_steps because current values unknown)
-    pwm.set_pwm(channel_arm_above, 0, robot_arm_functions.angle_to_pulse_arm(base_arm_above))
-    pwm.set_pwm(channel_arm_under, 0, robot_arm_functions.angle_to_pulse_arm(base_arm_under))
+    robot_arm_functions.directly_back_to_base()
 
     while True:
         angle_arm_under = input("Enter angle of lower arm (or q to quit): ")
@@ -67,13 +36,13 @@ def main():
 
         # Move
         # arm above
-        robot_arm_functions.go_arm_by_steps(channel_arm_above, current_angle_arm_above, angle_arm_above)
+        robot_arm_functions.go_arm2_by_steps(current_angle_arm_above, angle_arm_above)
         current_angle_arm_above = angle_arm_above
         # arm under
-        robot_arm_functions.go_arm_by_steps(channel_arm_under, current_angle_arm_under, angle_arm_under)
+        robot_arm_functions.go_arm1_by_steps(current_angle_arm_under, angle_arm_under)
         current_angle_arm_under = angle_arm_under
         # gripper
-        pwm.set_pwm(channel_gripper, 0, robot_arm_functions.angle_to_pulse_gripper(angle_gripper))
+        robot_arm_functions.gripper(angle_gripper)
 
 
 if __name__ == "__main__":
--- robot_arm_functions.py
+++ robot_arm_functions.py
@@ -1,6 +1,52 @@
 # Robot Arm Functions
 # Author: GuustFlater13
 
+# Import the PCA9685 module.
+import time
+
+import Adafruit_PCA9685
+
+# Configure min and max servo pulse lengths
+# MG946R
+servo_min_mg946r = 90  # Min pulse length out of 4096
+servo_max_mg946r = 600  # Max pulse length out of 4096
+
+# SG60
+servo_min_sg90 = 146  # Min pulse length out of 4096
+servo_max_sg90 = 600  # Max pulse length out of 4096
+
+channel_gripper = 3
+channel_arm_under = 7
+channel_arm_above = 11
+
+base_arm_under = 20
+base_arm_above = 50
+
+max_arm_under = 100
+max_arm_above = 120
+
+# Initialise the PCA9685 using the default address (0x40)
+# Alternatively specify a different address and/or bus:
+# pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
+pwm = Adafruit_PCA9685.PCA9685()
+
+# Set frequency to 60hz, good for servos.
+pwm.set_pwm_freq(60)
+
+
+def get_base_arm_under():
+    return base_arm_under
+
+
+def get_base_arm_above():
+    return base_arm_above
+
+
+# If not on Base, Go direct to base (not using go_arm_by_steps because current values unknown)
+def directly_back_to_base():
+    pwm.set_pwm(channel_arm_above, 0, angle_to_pulse_arm(base_arm_above))
+    pwm.set_pwm(channel_arm_under, 0, angle_to_pulse_arm(base_arm_under))
+
 
 # calculate pule Width
 def angle_to_pulse_gripper(angle):
@@ -19,8 +65,23 @@ def angle_to_pulse_arm(angle):
 
 
 # go step by step
-def go_arm_by_steps(channel, current_angle, angle):
+def go_arm1_by_steps(current_angle, angle):
     step = int(current_angle)
+    channel = channel_arm_under
+    while step != int(angle):
+        if int(current_angle) < int(angle):
+            step = step + 1
+            pwm.set_pwm(channel, 0, angle_to_pulse_arm(step))
+        if int(current_angle) > int(angle):
+            step = step - 1
+            pwm.set_pwm(channel, 0, angle_to_pulse_arm(step))
+        print("step: ", step)
+        time.sleep(.05)
+
+
+def go_arm2_by_steps(current_angle, angle):
+    step = int(current_angle)
+    channel = channel_arm_above
     while step != int(angle):
         if int(current_angle) < int(angle):
             step = step + 1
@@ -35,6 +96,11 @@ def go_arm_by_steps(channel, current_angle, angle):
 # Go to base
 def go_to_base(current_angle_arm_under, current_angle_arm_above):
     print("Going back to base, from: ", current_angle_arm_under, "and", current_angle_arm_above, )
-    go_arm_by_steps(channel_arm_under, current_angle_arm_under, base_arm_under)
-    go_arm_by_steps(channel_arm_above, current_angle_arm_above, base_arm_above)
-    pwm.set_pwm(channel_gripper, 0, angle_to_pulse_gripper(0))
+    go_arm1_by_steps(current_angle_arm_under, base_arm_under)
+    go_arm2_by_steps(current_angle_arm_above, base_arm_above)
+    gripper(0)
+
+
+# gripper
+def gripper(angle):
+    pwm.set_pwm(channel_gripper, 0, angle_to_pulse_gripper(angle))
